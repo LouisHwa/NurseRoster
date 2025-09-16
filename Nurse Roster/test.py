4d300b8049ade3c0e58865ad27a5dd1838dcc6c5
LouisHwa <hwalouis888@gmail.com>
2025-09-16 15:06:09 +0800

    Update test.py

--- Nurse Roster/test.py
+++ Nurse Roster/test.py
@@ -1,7 +1,7 @@
 import json 
 from ortools.sat.python import cp_model
 
-with open("data/nurse.json", "r") as f:
+with open("Nurse Roster/data/nurse.json", "r") as f:
     nurses = json.load(f)
 
 
@@ -35,6 +35,33 @@ for nurse in nurses:
             for day in days 
             for slot in time_slots) <= 2
     )
+
+total_shifts = len(days) * len(time_slots)
+min_shifts = total_shifts // len(nurses)      # floor
+max_shifts = -(-total_shifts // len(nurses))  # ceil
+
+for nurse in nurses:
+    total_nurse_shifts = sum(
+        assignment[(nurse["nurse_id"], day, slot)]
+        for day in days for slot in time_slots
+    )
+    model.Add(total_nurse_shifts >= min_shifts)
+    model.Add(total_nurse_shifts <= max_shifts)
+
+
+avg = total_shifts / len(nurses)
+fairness_terms = []
+for nurse in nurses:
+    total_nurse_shifts = sum(
+        assignment[(nurse["nurse_id"], day, slot)]
+        for day in days for slot in time_slots
+    )
+    # Penalize deviation from average workload
+    diff = model.NewIntVar(-21, 21, f"diff_{nurse['nurse_id']}")
+    abs_diff = model.NewIntVar(0, 21, f"abs_diff_{nurse['nurse_id']}")
+    model.Add(diff == total_nurse_shifts - int(avg))
+    model.AddAbsEquality(abs_diff, diff)
+    fairness_terms.append(abs_diff)
 
 # Objective: preferences
 preference_terms = []
